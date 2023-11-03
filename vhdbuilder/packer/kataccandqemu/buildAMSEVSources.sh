2eedbec1144c8d84ffd4c92d3e2f99511c40cf2d
arc9693 <archana1@microsoft.com>
2023-11-03 05:39:26 +0000

    Add fallback as errors from this line don't really matter

--- vhdbuilder/packer/kataccandqemu/buildAMSEVSources.sh
+++ vhdbuilder/packer/kataccandqemu/buildAMSEVSources.sh
@@ -3,9 +3,9 @@ set -ex
 
 sudo dnf install -y libcap-ng-devel libattr-devel liburing-devel
 git clone https://github.com/AMDESE/AMDSEV.git --branch=sev-snp-devel AMDSEV
-DEST="/usr/local"
+DEST="$HOME/AMDSEV/usr/local"
 pushd AMDSEV
-sudo ./build.sh qemu --install $DEST
+sudo ./build.sh qemu --install $DEST || true
 sudo ./build.sh ovmf --install $DEST
 pushd qemu
 ./configure --enable-virtfs --disable-virtiofsd --target-list=x86_64-softmmu --enable-debug --prefix=$DEST --enable-linux-io-uring
